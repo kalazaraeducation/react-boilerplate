a011d910b3c415c099258590339183dd06bbc36e
Aditya Yadav <adiyadav.902@gmail.com>
2022-10-23 17:29:08 +0530

    23-10-2022-05-28-PM

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./index.css";
+import AppRoutes from "./Routes";
+import { BrowserRouter as Router } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <AppRoutes />
+      </Router>
+    </>
+  );
+}
+
+export default App;
